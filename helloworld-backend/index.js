7f8ab4daddd8047e7ccbbf333553fa0b67818671
Robert Pandele <robpan38@DESKTOP-BRIV46P.localdomain>
2022-03-12 18:21:10 +0200

    course examples

---
+++ helloworld-backend/index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2022-05-30T17:30:31.098Z",
+        important: true,
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2022-05-30T18:39:34.091Z",
+        important: false,
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true,
+    },
+];
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>");
+});
+
+/**
+ * get all notes
+ */
+
+app.get("/api/notes", (request, response) => {
+    response.json(notes);
+});
+
+/**
+ * get a single note
+ */
+
+app.get("/api/notes/:id", (request, response) => {
+    const id = request.params.id;
+    const note = notes.find((note) => note.id === Number.parseInt(id));
+
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+/**
+ * delete a single note
+ */
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number.parseInt(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+
+    response.status(204).end();
+});
+
+/**
+ * add a single note
+ */
+
+const generateId = () => {
+    const id = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+    return id + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body;
+
+    if(!body.content) {
+        return response.status(404).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = [...notes, note];
+
+    response.send(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
